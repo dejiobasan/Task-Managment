6e4c6eed28a55c30ab31e83da4f6ae718883ab78
dejiobasan <dejiobasan@yahoo.com>
2024-01-26 17:22:48 +0100

    Boiler plates done; Created folders and script files to begin writing codes '-'

---
+++ Backend/Server.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.TaskDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully!");
+});
+
+
+app.listen(port, function () {
+    console.log("server started at port 5000.");
+});
