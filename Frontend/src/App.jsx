ee87e9e284e29688b8f4186fead79799378af797
dejiobasan <dejiobasan@yahoo.com>
2024-03-25 19:43:12 +0100

    Created Components files and configured Tailwind.css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar"
+import Home from "../src/components/Home"
 
 function App() {
-
-
   return (
-    <>
-    </>
+    <Router> 
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            
+        );
+    }
+};
