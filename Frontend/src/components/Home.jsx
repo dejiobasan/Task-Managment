883cfd9a10d8ac62e5e85c18d817e263c1d6d147
dejiobasan <dejiobasan@yahoo.com>
2024-05-06 12:37:08 +0100

    created static forms for creating a User

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,10 +1,100 @@
+import { useState } from "react";
+import { userNavigate } from "react-router-dom"
 
 function Home() {
+  const navigate = userNavigate();
+  const [newUser, setNewUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setNewUser((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(newUser);
+    //axios post request
+    navigate("/signin");
+  }
+
   return (
-    <div className="">
-    Home
-    </div>
-  )
+    <>
+      <div className="container mx-auto p-6">
+        <h1 className="text-5xl font-bold">Welcome to Task IT</h1>
+        <p className="text-2xl text-left">
+          This is a simple task management application, Create an Account to
+          continue.
+        </p>
+      </div>
+
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="../src/assets/Images/Logo.png"
+            alt="Your Company"
+          />
+        </div>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign Up
+        </h2>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Create a Username:
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="email"
+                  value={newUser.username}
+                  onChange={handleChange}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Create a Password:
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={newUser.password}
+                  onChange={handleChange}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- Frontend/src/components/SignIn.jsx
+++ Frontend/src/components/SignIn.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
+
 
 function SignIn() {
+
+
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  }
+
   return (
-    <div>SignIn</div>
+   
   )
 }
 
