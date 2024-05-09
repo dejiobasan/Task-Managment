88eb053f1f44e048efa4b1fd740d66f89600696b
dejiobasan <dejiobasan@yahoo.com>
2024-05-09 15:44:41 +0100

    .

--- Frontend/src/components/CreateTask.jsx
+++ Frontend/src/components/CreateTask.jsx
@@ -1,7 +1,34 @@
+import { useState  } from "react";
+
 
 function CreateTask() {
+
+  const [task, setTask] = useState({
+    username: "",
+    title: "",
+    description: "",
+    category: "",
+    duration: "",
+    startdate: "",
+    duedate: "",
+    status: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setTask((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(task);
+    //axios post request then redirect to createTask page
+  }
+ 
   return (
-    <div>CreateTask</div>
+    <div>
+      
+    </div>
   )
 }
 
