ad869dc7ee1794434aa13b49c0c648988218433b
dejiobasan <dejiobasan@yahoo.com>
2024-07-05 16:51:36 +0100

    .

--- Frontend/src/components/CreateTask.jsx
+++ Frontend/src/components/CreateTask.jsx
@@ -26,8 +26,106 @@ function CreateTask() {
   }
  
   return (
-    <div>
-      
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <form onSubmit={handleSubmit}>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Task Profile</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Create Task and Save Your Task.
+            </p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  Username:
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      id="username"
+                      name="username"
+                      type="text"
+                      autoComplete="username"
+                      value={task.username}
+                      onChange={handleChange}
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">
+                  Title:
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      id="title"
+                      name="title"
+                      type="text"
+                      autoComplete="title"
+                      value={task.title}
+                      onChange={handleChange}
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
+                  Description:
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="description"
+                    name="description"
+                    rows={5}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    defaultValue={''}
+                    value={task.description}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about the Task.</p>
+              </div>
+            </div>
+
+            <div className="border-b border-gray-900/10 pb-12">
+                <div className="sm:col-span-3">
+                  <label htmlFor="category" className="block text-sm font-medium leading-6 text-gray-900">
+                    Category
+                  </label>
+                  <div className="mt-2">
+                    <select
+                      id="category"
+                      name="category"
+                      autoComplete="category-name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                      <option>New</option>
+                      <option>Standby</option>
+                      <option>Completed</option>
+                    </select>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Save
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
